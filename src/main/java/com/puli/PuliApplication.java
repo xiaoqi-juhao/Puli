dd380186292fea1d6a86e0b297f55202beefa7ee
xiaoqi_douhao <512823qixue>
2017-03-23 16:58:22 +0800

    2017年3月23日16:58:15

---
+++ src/main/java/com/puli/PuliApplication.java
@@ -0,0 +1,12 @@
+package com.puli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuliApplication.class, args);
+	}
+}
