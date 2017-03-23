1da3a8b1c50f61e83056c402120fc74bf9ca0758
xiaoqi_douhao <512823qixue>
2017-03-23 17:09:37 +0800

    2017年3月23日17:09:32

--- src/main/java/com/puli/PuliApplication.java
+++ src/main/java/com/puli/PuliApplication.java
@@ -2,10 +2,17 @@ package com.puli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PuliApplication {
 
+	@RequestMapping("/")
+	public String getStr(){
+		return "Hello word!!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PuliApplication.class, args);
 	}
